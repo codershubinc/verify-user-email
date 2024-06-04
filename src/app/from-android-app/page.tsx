5fe4ef4c2857175af9aafdce3dbe76ad877cea16
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-06-05 00:40:53 +0530

    update page.tsx

--- src/app/from-android-app/page.tsx
+++ src/app/from-android-app/page.tsx
@@ -1,13 +1,13 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import userVerify from "../../../db/from-android-app-email-verification.auth";
+
 
 
 export default function Home() {
   const [error, setError] = useState('')
-  const urlParams = new URLSearchParams(window.location.search);
-  const secret = urlParams.get('secret');
-  const userId = urlParams.get('userId');
+  const [secret, setSecret] = useState('')
+  const [userId, setUserId] = useState('')
   const verifyUserEmail = () => {
 
     try {
@@ -26,6 +26,13 @@ export default function Home() {
       setError(error?.message)
     }
   };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const secret = String(urlParams.get('secret'));
+    const userId = String(urlParams.get('userId'));
+    setSecret(secret)
+    setUserId(userId)
+  }, [])
   return (
     <>
       {error && <p>{error}</p>}
